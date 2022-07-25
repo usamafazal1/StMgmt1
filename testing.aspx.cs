3531e9761cc80871aea0ea344d704b7486a5a1ab
Usama Fazal <Usama.Fazal@outlook.com>
2022-07-25 05:41:32 -0400

    Add project files.

---
+++ testing.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace testing
+{
+    public partial class testing : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ViewState["Value"] = TextBox1.Text;
+            TextBox1.Text = "0";
+            if (IsPostBack)
+            {
+                int num = Convert.ToInt32(ViewState["Value"]);
+                int num1 = num + 1;
+                TextBox1.Text = num1.ToString();
+            }
+        }
+
+    }
+}
